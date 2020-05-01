4af992c0eac335ed908d77df3a744c40456d8e31
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-01 11:17:25 -0500

    added ref to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_160516) do
+ActiveRecord::Schema.define(version: 2020_05_01_161542) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
