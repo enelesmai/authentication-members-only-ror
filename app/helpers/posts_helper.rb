d75d959f4a7f9c6b696fb768de2389bc18132d78
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-04 14:58:59 -0500

    Move author logic from view to post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+    def post_author(post)
+        if user_signed_in?
+            return post.user.name
+        else
+            return "Authenticate to view author"
+        end
+    end
 end
